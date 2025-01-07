8a373ecb9e64632c956c4e440d2eb219707871c1
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-01-07 12:37:47 -0800

    Finished ListPractice

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,27 +1,53 @@
+import java.util.ArrayList;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
+    ArrayList<String> stringList = new ArrayList<String>();
 
     // Add 3 elements to the list (OK to do one-by-one)
+    stringList.add("Creed");
+    stringList.add("Michael");
+    stringList.add("Andy");
 
     // Print the element at index 1
+    System.out.println(stringList.get(1));
 
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
+    stringList.set(1, "Jim");
+    System.out.println(stringList.get(1));
 
     // Insert a new element at index 0 (the length of the list will change)
+    stringList.add(0, "Dwight");
+    System.out.println(stringList.get(0));
 
     // Check whether the list contains a certain string
+    if(stringList.contains("Kevin")){
+      System.out.println(true);
+    }else{
+      System.out.println(false);
+    }
 
     // Iterate over the list using a traditional for-loop.
     // Print each index and value on a separate line
+    for(int i = 0; i < stringList.size(); i++){
+      System.out.println(stringList.get(i));
+    }
 
     // Sort the list using the Collections library
+    stringList.sort(null);
+
 
     // Iterate over the list using a for-each loop
     // Print each value on a second line
+    for(int i = 0; i < stringList.size(); i++){
+      System.out.println(stringList.get(i));
+      System.out.println();
+
+    }
 
     /*
      * Usage tip!
